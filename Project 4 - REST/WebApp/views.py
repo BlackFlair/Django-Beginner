8a1c37afca7109a275abb2efc6175d5ddc8d9854
Surin Machaiah <surinmachaiah10@gmail.com>
2021-06-09 15:53:25 +0530

    09-Jun-2021

---
+++ Project 4 - REST/WebApp/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .models import Employee
+from .serializers import EmployeesSerializer
+
+# Create your views here.
+
+class EmployeeList(APIView):
+    def get(self, request):
+        emp1 = Employee.objects.all()
+        serializer = EmployeesSerializer(emp1, many=True)
+        return Response(serializer.data)
